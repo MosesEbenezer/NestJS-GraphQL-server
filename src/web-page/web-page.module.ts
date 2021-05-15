cb6b6ea728040c0a7aada1743565bdc53ec8077d
Moses Ebenezer <moseschimdike@gmail.com>
2021-05-15 21:48:44 +0100

    resolver

--- src/web-page/web-page.module.ts
+++ src/web-page/web-page.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { WebPageResolver } from './web-page.resolver';
+import { WebPageService } from './web-page.service';
 
-@Module({})
+@Module({
+  providers: [WebPageResolver, WebPageService],
+})
 export class WebPageModule {}
